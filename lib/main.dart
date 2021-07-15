476e1838cdeebf261bdcc423f8465b48d8e5c612
Mohammad Ashour <ashour.mohammad@gmail.com>
2021-07-15 13:09:13 +0100

    Adding localization libraries and config

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_phrase_ota_2021/features/hereos/hero_list.dart';
 
 void main() => runApp(MyApp());
@@ -8,6 +9,15 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Heroes of Computer Science',
+      localizationsDelegates: [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: [
+        Locale('en', ''),
+        Locale('ar', ''),
+      ],
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
