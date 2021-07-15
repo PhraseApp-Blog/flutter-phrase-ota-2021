86e2486905e9296d3e1dd93cca293e52da3fb877
Mohammad Ashour <ashour.mohammad@gmail.com>
2021-07-15 17:12:47 +0100

    Connecting to Phrase OTA

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:phrase/phrase.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:flutter_gen/gen_l10n/phrase_localizations.dart';
+import 'package:flutter_phrase_ota_2021/services/config/config.dart';
 import 'package:flutter_phrase_ota_2021/features/hereos/hero_list.dart';
 
-void main() => runApp(MyApp());
+Future<void> main() async {
+  await Config.load();
+
+  Phrase.setup(
+    Config.phraseOtaDistributionId!,
+    Config.phraseOtaSecret!,
+  );
+
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       onGenerateTitle: (context) => AppLocalizations.of(context)!.appTitle,
-      localizationsDelegates: AppLocalizations.localizationsDelegates,
-      supportedLocales: AppLocalizations.supportedLocales,
+      localizationsDelegates: PhraseLocalizations.localizationsDelegates,
+      supportedLocales: PhraseLocalizations.supportedLocales,
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
---
+++ lib/services/config/config.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import './env.dart';
+
+class Config {
+  static const String fileName = '.env';
+
+  static Future load() async => await dotenv.load(fileName: fileName);
+
+  static String? get(String key) => dotenv.env[key];
+
+  static Env get env {
+    if (kDebugMode) {
+      return Env.Debug;
+    } else if (kReleaseMode) {
+      return Env.Release;
+    } else {
+      return Env.Unknown;
+    }
+  }
+
+  static String? get phraseOtaDistributionId =>
+      get('PHRASE_OTA_DISTRIBUTION_ID');
+
+  static String? get phraseOtaSecret {
+    if (env == Env.Debug) {
+      return get('PHRASE_OTA_DEV_SECRET');
+    } else if (env == Env.Release) {
+      return get('PHRASE_OTA_PRODUCTION_SECRET');
+    }
+  }
+}
